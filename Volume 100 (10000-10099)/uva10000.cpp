18fd364d8bf56ba2efd97aaea7b09ba23eafb9e4
JayTang <abow1216@hotmail.com>
2016-05-10 12:40:52 +0800

    10000 - Longest Paths

--- Volume 100 (10000-10099)/uva10000.cpp
+++ Volume 100 (10000-10099)/uva10000.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 // return pair<point n, longest distance from start to point n>
 pair<int, int> travel(pair<vector<int>, int> *path, int from, int distance) {
-	// pruning sub path, to avoid time limit exceeded 
+	// pruning sub path, to avoid time limit exceeded
 	if (path[from].second > distance) return pair<int, int>(from, path[from].second);
 
 	path[from].second = distance;
 
-	pair<int, int> longest_path(from, path[from].second), tmp;
+	pair<int, int> longest(from, path[from].second), current;
 	for (int i: path[from].first) {
-		tmp = travel(path, i, distance + 1);
-		if (tmp.second > longest_path.second) longest_path = tmp;
-		else if (tmp.second == longest_path.second && tmp.first < longest_path.first) longest_path.first = tmp.first;
+		current = travel(path, i, distance + 1);
+		if (current.second > longest.second) longest = current;
+		else if (current.second == longest.second && current.first < longest.first) longest.first = current.first;
 	}
 
-	return longest_path;
+	return longest;
 }
 
 int main() {
@@ -33,7 +33,7 @@ int main() {
 			path[from].second = 0;
 		}
 
-		// depth first search 
+		// depth first search
 		pair<int, int> longest_path = travel(path, spoint, 0);
 		cout << "Case " << ncase++ << ": The longest path from " << spoint << " has length " << longest_path.second << ", finishing at " << longest_path.first << "." << endl << endl;
 	}
